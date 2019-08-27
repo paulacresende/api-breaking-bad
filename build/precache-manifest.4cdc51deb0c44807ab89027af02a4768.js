6e7a4eb92add58682909aa421a2c49c6194c0c14
paulacresende <pcarvalhoresende@gmail.com>
2019-08-27 09:15:06 -0300

    build

---
+++ build/precache-manifest.4cdc51deb0c44807ab89027af02a4768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0256283ba51a2bbb2624dafa11e13479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "263bed4c908e92c855a4",
+    "url": "/static/css/main.e4e8bba2.chunk.css"
+  },
+  {
+    "revision": "efa4b5cdc350542df075",
+    "url": "/static/js/2.a314e987.chunk.js"
+  },
+  {
+    "revision": "263bed4c908e92c855a4",
+    "url": "/static/js/main.582c5748.chunk.js"
+  },
+  {
+    "revision": "42407d508fd26330997b",
+    "url": "/static/js/runtime~main.c2b67788.js"
+  },
+  {
+    "revision": "e159f1a20614bf5fba21dc64eb5f8160",
+    "url": "/static/media/breaking-bad-vector-logo.e159f1a2.png"
+  },
+  {
+    "revision": "d38e4510327762e101df53a951bf5b7b",
+    "url": "/static/media/estrela.d38e4510.png"
+  }
+]);
