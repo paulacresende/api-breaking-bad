d976a7ad78ed233f3dddb508546c0fdf853736b4
paulacresende <pcarvalhoresende@gmail.com>
2019-08-27 09:18:19 -0300

    v2

--- src/App.js
+++ src/App.js
@@ -2,46 +2,62 @@ import React, { Component } from 'react';
 import api from '../src/services/api.js';
 import './App.css';
 import Cards from './components/Cards.js';
+import logo from './assets/breaking-bad-vector-logo.png';
+import lupa from './assets/ic-search-copy.png';
 
 class App extends Component {
 
-  state = {
-    characters: [],
-    search: " ",
-    stateSearch: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: [],
+      search: " ",
+      searchBar: false,
+    };
   }
 
   componentDidMount() {
-     api.get('/characters')
+    api.get('/characters')
       .then((response) => {
-        //console.log(response)
-        this.setState({ 
+        console.log(response)
+        this.setState({
           characters: response.data
         });
       })
       .catch((error) =>
         console.log(error)
-      );   
+      );
   }
 
-  onchange = e =>{
-    this.setState({ search : e.target.value})
+  onchange = e => {
+    this.setState({ search: e.target.value })
+    this.setState({ searchBar: true })
   }
 
   render() {
 
-    const {search} = this.state;
-    const filterCharacteres = this.state.characters.filter ( character => {
-      return character.name.toLowerCase().indexOf( search.toLowerCase() ) !== -1
+    const { search } = this.state;
+    const filterCharacteres = this.state.characters.filter(character => {
+      return character.name.toLowerCase().indexOf(search.toLowerCase()) !== -1
     })
 
     return (
       <div className="App">
+        <nav className="header">
+          <img src={logo} alt='logo' id="logo" />
+          <div className="search-bar">
+            <input type="text" name="Pesquisar" placeholder="Pesquise os personagens" className="input" onChange={this.onchange} />
+            <img src={lupa} alt="lupa" />
+          </div>
+        </nav>
+        
         <div>
-          <input label = "Pesquise os personagens" onChange = {this.onchange} />
+          {this.state.searchBar ?
+            <h1>Você pesquisou por "{search}"</h1>
+            : <h1>Personagens</h1> 
+          }
         </div>
-        <h1>Voce pesquisou por "{search}"</h1>
-        <div>
+        <div className="all-characters">
           {
             filterCharacteres.map((character) => (
               <Cards character={character} key={character.char_id} />
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './Cards.css';
+import estrela from '../assets/estrela.png'
 
-const Cards = ({character}) => {
-   // console.log(card);
-    return (
-        <div className="card">
-          <div className="card-img">
-          <img src={character.img} alt="imagem" className="hero-img"/>
-          </div>
-          <div className="card-description">
-          <h3>{character.name}</h3>
-          <p>{character.status}</p>
-          <p>{character.occupation}</p>
-          </div>
-
+const Cards = ({ character }) => {
+  // console.log(card);
+  return (
+    <div className="character">
+      <img src={character.img} alt="imagem-character" />
+      <div className={`character-status ${character.status === `Alive` ? `alive` : 'dead'}`}>
+        <p>{character.status}</p>
+      </div>
+      <div className="character-description">
+        <h3>{character.name}</h3>
+        <div className="birthday">
+          <img src={estrela} alt="birthday" />
+         <p>{character.birthday}</p>
         </div>
-    );
+        <p>{character.occupation[0]} <br /> {character.occupation[1]}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Cards;
